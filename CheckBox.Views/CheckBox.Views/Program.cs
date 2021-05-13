86d4a8854d7e34f7cf04a5ebecff6df5e71ab798
awaynemd <awaynemd@yahoo.com>
2021-05-12 19:55:26 -0500

    Initial Setup from Samples -- FAILS

---
+++ CheckBox.Views/CheckBox.Views/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static CheckBox.Program;
+
+namespace CheckBox.Views
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main() =>
+         CheckBox.Program.main(new MainWindow(), () => new Window1(), () => new Window2());
+    }
+}
